0e9d53bf7fb4439bfa9b02829a907a053ceb09af
Jaden Geller <jaden.geller@gmail.com>
2013-12-29 19:30:20 -0800

    Initial empty project

---
+++ JGDictionaryLayoutViewExample/JGDictionaryLayoutViewExample/JGViewController.m
@@ -0,0 +1,29 @@
+//
+//  JGViewController.m
+//  JGDictionaryLayoutViewExample
+//
+//  Created by Jaden Geller on 12/29/13.
+//  Copyright (c) 2013 Jaden Geller. All rights reserved.
+//
+
+#import "JGViewController.h"
+
+@interface JGViewController ()
+
+@end
+
+@implementation JGViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ JGDictionaryLayoutViewExample/JGDictionaryLayoutViewExampleTests/JGDictionaryLayoutViewExampleTests.m
@@ -0,0 +1,34 @@
+//
+//  JGDictionaryLayoutViewExampleTests.m
+//  JGDictionaryLayoutViewExampleTests
+//
+//  Created by Jaden Geller on 12/29/13.
+//  Copyright (c) 2013 Jaden Geller. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface JGDictionaryLayoutViewExampleTests : XCTestCase
+
+@end
+
+@implementation JGDictionaryLayoutViewExampleTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
